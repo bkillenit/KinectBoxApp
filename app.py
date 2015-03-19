59174f1470319f8f37a082406f34b1f1f22dfb4d
bkillenit <blaine.j.killen@gmail.com>
2015-03-19 06:45:51 -0500

    isolating test to socket com

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import rpyc
-import ableton
+# import ableton
 import socket
 import sys
 
@@ -18,7 +18,7 @@ except socket.error as msg:
     sys.exit()
      
 print 'Socket bind complete'
- 
+
 #Start listening on socket
 s.listen(10)
 print 'Socket now listening'
@@ -26,27 +26,27 @@ print 'Socket now listening'
 
 # initializing python objects to traverse down object tree and find us useful objects
 c = rpyc.connect('localhost', 17744)
-abletonApp = c.root.Live.Application.get_application()
-doc = abletonApp.get_document()
+# abletonApp = c.root.Live.Application.get_application()
+# doc = abletonApp.get_document()
 
-mixer_device = doc.tracks[0].mixer_device
-return_tracks = doc.return_tracks
-tracks = doc.tracks
+# mixer_device = doc.tracks[0].mixer_device
+# return_tracks = doc.return_tracks
+# tracks = doc.tracks
 
-ableton.initializeAbletonData(doc.tempo, mixer_device.sends, return_tracks, tracks, doc.current_song_time, doc.is_playing)
+# ableton.initializeAbletonData(doc.tempo, mixer_device.sends, return_tracks, tracks, doc.current_song_time, doc.is_playing)
 
-tempo_callback = lambda: ableton.tempoChange(doc.tempo, doc.current_song_time)
-effect_callback = lambda: ableton.effectChange(mixer_device.sends, return_tracks, doc.current_song_time)
-track_callback = lambda: ableton.trackChange(tracks, doc.current_song_time)
-music_callback = lambda: ableton.musicChange(doc.is_playing, doc.current_song_time)
+# tempo_callback = lambda: ableton.tempoChange(doc.tempo, doc.current_song_time)
+# effect_callback = lambda: ableton.effectChange(mixer_device.sends, return_tracks, doc.current_song_time)
+# track_callback = lambda: ableton.trackChange(tracks, doc.current_song_time)
+# music_callback = lambda: ableton.musicChange(doc.is_playing, doc.current_song_time)
 
-# adding all of the listeners for Ableton
-for send in mixer_device.sends:
-	send.add_value_listener(effect_callback)
-doc.add_tempo_listener(tempo_callback)
-for track in tracks:
-	track.add_playing_slot_index_listener(track_callback)
-doc.add_is_playing_listener(music_callback)
+# # adding all of the listeners for Ableton
+# for send in mixer_device.sends:
+# 	send.add_value_listener(effect_callback)
+# doc.add_tempo_listener(tempo_callback)
+# for track in tracks:
+# 	track.add_playing_slot_index_listener(track_callback)
+# doc.add_is_playing_listener(music_callback)
 
 try:
 	while True:
@@ -57,9 +57,10 @@ try:
 finally:
 	# removing all of the listeners after an error occurs or the kill signal is called in the terminal
 	print "\nlisteners unbinding"
-	doc.remove_tempo_listener(tempo_callback)
-	for send in mixer_device.sends:
-		send.remove_value_listener(effect_callback)
-	for track in tracks:
-		track.remove_playing_slot_index_listener(track_callback)
-	doc.remove_is_playing_listener(music_callback)
+	# doc.remove_tempo_listener(tempo_callback)
+	# for send in mixer_device.sends:
+	# 	send.remove_value_listener(effect_callback)
+	# for track in tracks:
+	# 	track.remove_playing_slot_index_listener(track_callback)
+	# doc.remove_is_playing_listener(music_callback)
+	s.close()
